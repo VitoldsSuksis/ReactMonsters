c4f05f7b95afe83770aa9c34d8ed0e479368663e
Vitolds <vsuksis@gmail.com>
2020-02-27 11:06:05 +0200

    Updates

---
+++ precache-manifest.700f591ebfb3c818aaa94c829970496e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0793b95ed0b8936140c49467e879637",
+    "url": "/ReactMonsters/index.html"
+  },
+  {
+    "revision": "b9a21fda32e256c658d9",
+    "url": "/ReactMonsters/static/css/main.c02da868.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/ReactMonsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactMonsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a21fda32e256c658d9",
+    "url": "/ReactMonsters/static/js/main.ffb2f564.chunk.js"
+  },
+  {
+    "revision": "17a8487f77332344e279",
+    "url": "/ReactMonsters/static/js/runtime-main.233bec6d.js"
+  }
+]);
